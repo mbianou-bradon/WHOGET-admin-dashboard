795aa004bdf311e27476b35ec6b3aa54717759dc
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-12 09:01:31 +0100

    Updated axios baseURL

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Set config defaults when creating the instance
 const client = axios.create({
     // baseURL: 'https://whoget.onrender.com/api'
-    baseURL: 'http://127.0.0.1:4000/api'
+    baseURL: 'https://whoget.onrender.com/api'
 });
 
 export default client;
